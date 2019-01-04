4e932de26725883bf92c9575b452eb4cfe66eec3
Nrithya <31702989+Nrithya@users.noreply.github.com>
2019-01-04 16:31:47 +0530

    Update airline.r

--- airline.r
+++ airline.r
@@ -1,21 +1,26 @@
-airlineData=read.csv(file.choose(),header=T, sep=",")
+#import libraries
 library(cluster)
+
+#import data
+airlineData=read.csv(file.choose(),header=T, sep=",")
+
+#correlation
 cor(airlineData[,2:8])
+
+#scatterplot pairs
 pairs(airlineData[,2:5])
 pairs(airlineData[,c(2,6:8)])
+
+#labels
 labs= airlineData[,1]
-#entire set
-clusters=agnes(x=airlineData[,2:8], diss=FALSE,stand=TRUE, method="complete")
 
-DendClusters=as.dendrogram(clusters)
-rownames(DendClusters)=labs
-plot(DendClusters, labels=labs)
-
-#split set
+#agglomerative clustering
 clusters=agnes(x=airlineData[,c(2,6:8)], diss=FALSE,stand=TRUE, method="complete")
 DendClusters=as.dendrogram(clusters)
 plot(DendClusters)
 DendClusters$labels=airlineData$airline
+
+#divisive clustering
 model=diana(airlineData[,2:5],metric="manhattan", stand=TRUE)
 plot(model)
 identify(model)
