8d209d472e102cf520721f05a506f16356809020
Nrithya <31702989+Nrithya@users.noreply.github.com>
2019-01-04 13:05:12 +0530

    Update and rename Week1.r to LinearRegression.r

--- LinearRegression.r
+++ LinearRegression.r
@@ -1,8 +1,16 @@
-ratings=read.table(file.choose(), header=T, sep='\t')
-View(ratings)
-summary(ratings)
+#import libraries
 library(ggplot2)
+
+#import data
+ratings=read.table(file.choose(), header=T, sep='\t')
+summary(ratings)
+
+#correlation
 cor(ratings[2:5])
+
+#plot
 ggplot(ratings,aes(x=ambience,y=rating))+geom_point(color="orange")+stat_smooth(method="lm")
 ggplot(ratings,aes(x=(food+service+ambience),y=rating))+geom_point()+geom_smooth()
+
+#linear regression
 lm(rating~(food+service+ambience), ratings)
