725aa1bc881398512345a0efd9209481256de375
Nrithya <31702989+Nrithya@users.noreply.github.com>
2019-01-04 12:39:33 +0530

    Update DecisionTree.r

--- DecisionTree.r
+++ DecisionTree.r
@@ -1,9 +1,20 @@
-golf=read.csv(file.choose(),header=T,sep = ',')
+
+#import libraries
 library(rpart)
+
+#import data
+golf=read.csv(file.choose(),header=T,sep = ',')
+
+#set search path
 attach(golf)
+
+#decision tree
 model=rpart(PlayGolf~.,data=golf, method="class", control=rpart.control(minsplit=2, minbucket=1))
+
+#print & plot tree summary
 printcp(model)
 plotcp(model)
+
 plot(model, uniform=TRUE, main="Decision tree for golf data")
 text(model, use.n=TRUE, all=TRUE, cex=.8)
 
