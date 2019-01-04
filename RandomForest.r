8966f4afc61da8b7cd02e0514359f5550eea3e25
Nrithya <31702989+Nrithya@users.noreply.github.com>
2019-01-04 12:55:03 +0530

    Update and rename RF.r to RandomForest.r

--- RandomForest.r
+++ RandomForest.r
@@ -1,12 +1,24 @@
+#import library
+library(randomForest)
+
+#import data
 credit=read.csv(file.choose(),header=T,sep=",")
+
+#plot
 barplot(table(credit$DEFAULT))
 
+#sample
 set.seed(1234)
 population=sample(nrow(credit), 0.75*nrow(credit))
+
+#set test and train set
 train=credit[population,]
 test=credit[-population,]
-library(randomForest)
+
+#random forest
 model=randomForest(train$DEFAULT~., data=train)
+
+#prediction
 pred = predict(model, newdata=test)
 table(pred, test$DEFAULT)
-(21+3)/nrow(test)
+
